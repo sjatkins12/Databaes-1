e2344f91493be24f78428d4c0bb2160eefa77fa1
Steven Tu <nevets717@gmail.com>
2017-03-06 14:45:38 -0500

    Changed the database engine to MySQL. Use mysqlclient driver for connectivity to MySQL Database named mysql.

--- Databaes/settings.py
+++ Databaes/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Databaes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  	# Use the recommended django mysql adapter
+        'NAME': 'mysql',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',					# Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
