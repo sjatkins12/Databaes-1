8c9a3957d1309e6d72ed12fc0708420bff3beca2
Lawrence Qiu <lqiu96@gmail.com>
2017-03-06 15:51:54 -0500

    Created /crate html page (generic base) and /crate/[id] html page. Updated the url and views.

--- Crate/views.py
+++ Crate/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'base_generic.html')
+
+
+def box(request, box_id):
+    return render(request, 'index.html', {'box_id': box_id})
