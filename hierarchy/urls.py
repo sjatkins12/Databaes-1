7f2bff56c65b01a0fd1f61ca642fdb8003edecfd
Lawrence Qiu <lqiu96@gmail.com>
2017-03-08 22:08:49 -0500

    Homepage 'Explore' now shows possible categories. Added new categories landing page.

--- hierarchy/urls.py
+++ hierarchy/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # This must be first be otherwise it would match with subcategory
+    url(r'^$', views.category_list, name='list'),
+    url(r'^(?P<category_name>[\w]+)/$', views.category, name='category'),
+    # This must be before subcategory be otherwise it would match with subcategory
     url(r'^[\w]+/subcategory/[\w]+/interestgroup/(?P<interest_group_name>[\w]+)', views.interest_group,
         name='interest_group'),
-    url(r'^(?P<category_name>[\w]+)/$', views.category, name='category'),
     url(r'^[\w]+/subcategory/(?P<subcategory_name>[\w]+)', views.subcategory, name='subcategory'),
 ]
--- hierarchy/views.py
+++ hierarchy/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from .models import Category
+
 
 # Create your views here.
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'hierarchy/list.html', {'categories': categories})
+
+
 def category(request, category_name):
     return render(request, 'hierarchy/category.html', {'category': category_name})
 
