9a410a67758b1a1bcc53fec6237ea711db0c6a37
stevetu <nevets717@gmail.com>
2017-04-13 22:05:30 -0400

    Configured static files for use with collectstatic.

--- Databaes/settings.py
+++ Databaes/settings.py
@@ -16,8 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -80,7 +79,7 @@ TEMPLATES = [
 ]
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "homepage", 'static', 'homepage'),
 ]
 
 WSGI_APPLICATION = 'Databaes.wsgi.application'
