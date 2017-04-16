61c582d880f7ac4251098351e81f42a7472fdb44
Lawrence Qiu <lqiu96@gmail.com>
2017-04-16 17:34:22 -0400

    Changed urls.py regex to have more robust checking (against whitespace)

--- Crate/urls.py
+++ Crate/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     # Page that displays the list of categories
     url(r'^$', views.category_list, name='category_list'),
     # Page that displays the list of interest groups pertaining to the subcategory
-    url(r'^category/subcategory/(?P<subcategory_name>[\w]+)/$', views.subcategory, name='subcategory'),
+    url(r'^category/subcategory/(?P<subcategory_name>([\w]+)\s?)+/$', views.subcategory, name='subcategory'),
     # Page that displays the discussion for the given interest group
-    url(r'^category/subcategory/interest_group/(?P<interest_group_name>[\w]+)/$',
+    url(r'^category/subcategory/interest_group/(?P<interest_group_name>([\w]+)\s?)+/$',
         views.DiscussionFormView.as_view(), name='box_discussion'),
     # Page that allows users to vote on a box
     url(r'^(?P<box_id>[\d]+)/vote$', views.BoxVoteFormView.as_view(), name='box_item_vote'),
