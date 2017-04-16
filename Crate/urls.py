8ce36a4b03eac4db18c1847cdf1b8d3d63c93a76
Lawrence Qiu <lqiu96@gmail.com>
2017-04-16 17:28:53 -0400

    Fixed relative urls for categories and interest group pages.

--- Crate/urls.py
+++ Crate/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     # Page that displays the list of interest groups pertaining to the subcategory
     url(r'^category/subcategory/(?P<subcategory_name>[\w]+)/$', views.subcategory, name='subcategory'),
     # Page that displays the discussion for the given interest group
-    url(r'^category/subcategory/(?P<interest_group_name>[\w]+)/discussion$', views.DiscussionFormView.as_view(), name='box_discussion'),
+    url(r'^category/subcategory/interest_group/(?P<interest_group_name>[\w]+)/$',
+        views.DiscussionFormView.as_view(), name='box_discussion'),
     # Page that allows users to vote on a box
     url(r'^(?P<box_id>[\d]+)/vote$', views.BoxVoteFormView.as_view(), name='box_item_vote'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
